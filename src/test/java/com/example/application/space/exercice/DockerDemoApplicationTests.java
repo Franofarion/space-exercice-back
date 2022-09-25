da6ba1f4f765b2ef520b4405ad09b0ef0743c825
Baptiste Domange <baptiste.domange@gmail.com>
2022-09-25 21:28:36 +0200

    init

---
+++ src/test/java/com/example/application/space/exercice/DockerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.application.space.exercice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
